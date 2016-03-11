661acbf8b2eefa589e0a2d62c363a4c4dde294f1
Mauricio de Oliveira <mauricio@ucsd.edu>
2016-03-11 08:48:04 -0800

    Created NCUtil and its test. First function is NCConsistentQ

---
+++ NCAlgebra/NCUtil.m
@@ -0,0 +1,39 @@
+(* :Title: 	NCUtil.m *)
+
+(* :Author: 	mauricio *)
+
+(* :Context: 	NCUtil` *)
+
+(* :Summary: 
+*)
+
+(* :Alias:
+*)
+
+(* :Warnings: 
+*)
+
+(* :History:
+*)
+
+BeginPackage["NCUtil`",
+             "NonCommutativeMultiply`"];
+
+Clear[NCGrabSymbols];
+NCGrabSymbols::usage = "";
+
+Clear[NCConsistentQ];
+NCConsistentQ::usage = "";
+
+Begin["`Private`"];
+
+  NCGrabSymbols[exp_] := Union[Cases[exp, _Symbol, Infinity]];
+  NCGrabSymbols[exp_, pattern_] := 
+    Union[Cases[exp, (pattern)[_Symbol], Infinity]];
+
+  NCConsistentQ[exp_] := 
+    Length[Cases[exp, 
+            a_?NonCommutativeQ * b_?NonCommutativeQ, {0, Infinity}]] == 0;
+    
+End[];
+EndPackage[];
