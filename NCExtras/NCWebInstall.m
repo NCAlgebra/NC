ed16be66185cd8b4191ef18fee789472dd7a21f8
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-04-20 16:14:48 -0700

    NCWebUpdate

--- NCExtras/NCWebInstall.m
+++ NCExtras/NCWebInstall.m
@@ -29,12 +29,12 @@ Module[
      initfile, info, stream},
 
     Print["************************************************************************"];
-    Print["***                     Welcome to NCWebInstall!                     ***"];
+    Print["***    N C W e b I n s t a l l: a web autoinstaller for NCAlgebra    ***"];
     Print["************************************************************************"];
 
     (* Import NC_VERSION *)
     {label, version} = Import["https://raw.githubusercontent.com/NCAlgebra/NC/master/NC_VERSION"][[1, {1,2}]];
-    version = StringReplace[version, Whitespace -> ""];
+    version = StringTrim[version];
     Print["> This program will install ", label, " ", version];
     
     (* Import Unzip *)
@@ -47,24 +47,37 @@ Module[
          existing = DirectoryName[existing];
          {label, version} = Import[FileNameJoin[{existing, 
                                                 "NC_VERSION"}]][[1, {1,2}]];
-         version = StringReplace[version, Whitespace -> ""];
+         version = StringTrim[version];
          
          Print["\n> There seems to be an installation of"];
          Print["  ", label, " ", version];
          Print["  already in the directory"];
          Print["  ", existing];
          
-         (* Rename folder *)
+         (* Upgrade? *)
+         Print[];
+         Print["  Even though we recommend that you install on this same directory,"];
+         Print["  beware that installation will completely erase the directory contents."];
+         Print[];
+
          input = "Z";
          While[ !(input == "Y" || input == "N")
                ,
                 input = ToUpperCase[
-                   InputString["  Do you want to install on this same directory? (y/n) "]];
+                   InputString["  Do you want to install on this " <>
+                               "same directory, i.e. upgrade? (y/n) "]];
          ];
          If[ input == "Y"
             ,
              installdirectory = FileNameJoin[Drop[FileNameSplit[existing],-1]];
             ,
+             
+             Print[];
+             Print["  Having multiple installations of NCAlgebra can create conflicts"];
+             Print["  and we recommended that you remove the existing installation "];
+             Print["  before proceeding."];
+             Print[];
+             
              input = "Z";
              While[ !(input == "Y" || input == "N")
                    ,
@@ -81,15 +94,18 @@ Module[
                     Return[];
              ];
             ,
+             Print[];
              Print["  Proceeding without removing existing installation."];
-             Print["  You might want to rename the directory '", 
-                   existing, "' to avoid conflicts."];
+             Print["  You might want to rename the directory"];
+             Print["  ", existing];
+             Print["  to avoid conflicts."];
          ];
     ];
 
     (* Default installation path *)
     If[ !ValueQ[installdirectory],
-        installdirectory = FileNameJoin[{$UserBaseDirectory,"Applications"}]
+        installdirectory = FileNameJoin[{$UserBaseDirectory,"Applications"}];
+        installdirectory = $HomeDirectory;
     ];
     
     zipremote = "https://github.com/NCAlgebra/NC/archive/master.zip";
@@ -97,6 +113,7 @@ Module[
     Print["  ", zipremote];
     Print["  in the directory:"];
     Print["  ", installdirectory];
+         
     input = "Z";
     While[ !(input == "Y" || input == "N")
           ,
@@ -104,14 +121,46 @@ Module[
               InputString["  Do you want to change the installation directory? (y/n) "]];
     ];
     If[ input == "Y",
-        installdirectory = StringTrim[InputString["  Type installation directory: "]];
+        
+        Print["\n  Choose one of the options:"];
+        Print["  1) '", $HomeDirectory, "' (recommended)"];
+        Print["  2) '", 
+              FileNameJoin[{$UserBaseDirectory,"Applications"}],
+             "'"];
+        Print["  3) Type in my own directory"];
 
+        input = "Z";
+        While[ !(input == "1" || input == "2" || input == "3")
+              ,
+               input = StringTrim[InputString["  Choice? (1/2/3) "]];
+        ];
+    
+        installdirectory = 
+          Switch[ input
+                 ,
+                  "1"
+                 ,
+                  $HomeDirectory
+                 ,
+                  "2"
+                 ,
+                  FileNameJoin[{$UserBaseDirectory,"Applications"}]
+                 ,
+                  "3"
+                 ,
+                  (* type in *) 
+                 
+                  StringTrim[InputString["  Type installation directory: "]]
+        ];
+    
         Print["\n> This program will install the latest version of NCAlgebra from:"];
         Print["  ", zipremote];
         Print["  in the directory:"];
         Print["  ", installdirectory];
     ];
-         
+    
+    Print["\n> Last chance to change your mind."];
+    
     input = "Z";
     While[ !(input == "Y" || input == "N")
           ,
@@ -122,7 +171,6 @@ Module[
         Print["\n> Aborting..."];
         Return[];
     ];
-        
 
     (* Create directory if needed *)
     If[ !DirectoryQ[installdirectory],
@@ -140,12 +188,12 @@ Module[
     nclocal = FileNameJoin[{ installdirectory, "NC"}];
     Print["\n> Downloading:"];
     Print["  ", zipremote];
-    Print["  into directory:"];
+    Print["  into:"];
     Print["  ", ziplocal];
     
     (* get rid of previous download *)
     If[ FileExistsQ[ziplocal],
-        Print["\n> A local copy of the zip file already exists."];
+        Print["\n> A local copy of the installation zip file already exists."];
         Print["  Deleting '", ziplocal, "'."];
         Check[ DeleteFile@ziplocal;
               ,
@@ -175,7 +223,7 @@ Module[
     (* Download file *)
     fcfilesize = Unzip`URLFileByteSize[zipremote];
     Print[""];
-    Print["> Downloading ", ToString[Round[fcfilesize/1024^2]], " MB from:"];
+    Print["> Transfering ", ToString[Round[fcfilesize/1024^2]], " MB from:"];
     Print["  ", zipremote];
     Print["  Please be patient..."];
     Check[ Unzip`CopyRemote[zipremote, ziplocal];
@@ -195,27 +243,36 @@ Module[
     ];
     Print["  Done extracting files."];
 
-    Print["  Renaming downloaded folder."];
+    Print["  Removing downloaded installation file."];
+    Check[ DeleteFile@ziplocal
+          ,
+           Print["\n> Something went wrong.\n  Aborting..."];
+           Return[];
+    ];
+           
+    Print["  Renaming installation folder."];
     Check[ RenameDirectory[ dirlocal,
                             FileNameJoin[{ installdirectory, "NC"}] ];
           ,
            Print["\n> Something went wrong.\n  Aborting..."];
            Return[];
     ];
-          
+       
+    (* Go to $HomeDirectory *)
+    SetDirectory[$HomeDirectory];
+           
     (* Installled? *)
-    Print["\n> Installation successful?"];
     If [ FindFile[ "NC`" ] === $Failed
         ,
-         Print["  Directory"];
+         Print["> Post installation setup"];
+         Print["\n  Directory"];
          Print["  ", installdirectory];
-         Print["  does not to seem in the system $Path."];
+         Print["  does not to seem to be in the system $Path."];
          input = "Z";
          While[ !(input == "Y" || input == "N")
                ,
                 input = ToUpperCase[
-                   InputString["  Do you want NCWebInstall to add " 
-                               <> "this folder to the system $Path? (y/n) "]];
+                   InputString["  Do you want to add this folder to the system $Path? (y/n) "]];
          ];
          If[ input == "Y"
             ,
@@ -234,12 +291,16 @@ Module[
                           OpenAppend[initfile]
              ];
              Print["  Adding '", installdirectory, "' to $Path"];
-             WriteString[stream, "\n(* NCINSTALL - BEGIN *)\n"];
+             WriteString[stream, "\n(* NCALGEBRA - BEGIN *)\n"];
              WriteString[stream, "AppendTo[$Path,\"", installdirectory, "\"];\n"];
-             WriteString[stream, "(* NCINSTALL - END *)\n\n"];
+             WriteString[stream, "(* NCALGEBRA - END *)\n\n"];
              Close[stream];
+                   
+             Print["\n  Trying to import init.m"];
+             Get[initfile];
+                   
             ,
-             Print["  Proceeding without adding folder to $Path."];
+             Print["  Proceeding without adding installation folder to system $Path."];
              Print["  You may not be able to load NCAlgebra!"];
          ];
         ,
@@ -250,14 +311,14 @@ Module[
     Print["\n> Try to load NC.\n"];
     If [ FindFile["NC`"] =!= $Failed,
          Quiet[Needs["NC`"], Needs::nocont],
-         Print["Installation of NCAlgebra failed!"]
+         Print["Installation of NCAlgebra failed!"];
          Return[];
     ];
          
     Print["\n> Try to load NCAlgebra.\n"];
     If [ FindFile["NCAlgebra`"] =!= $Failed,
          Quiet[Needs["NCAlgebra`"], Needs::nocont],
-         Print["Installation of NCAlgebra failed!"]
+         Print["Installation of NCAlgebra failed!"];
          Return[];
     ];
     Print["\n> Congratulations, you have succesfully intalled NCAlgebra!"];
---
+++ NCExtras/NCWebUpdate.m
@@ -0,0 +1,87 @@
+(* :Title: 	NCWebUpdate *)
+
+(* :Author: 	Mauricio de Oliveira *)
+
+(* :Context: 	None *)
+
+(* :Summary:
+*)
+
+(* :Alias:
+*)
+
+(* :Warnings: 
+*)
+
+(* :History:
+*)
+
+If[ ("AllowInternetUse" /. SystemInformation["Network"]) === False,
+    Print["You have configured Mathematica not to access the internet. Too bad. 
+	Please check the \"Allow Mathematica to use the Internet\" box in the
+    Help \[FilledRightTriangle] Internet Connectivity dialog. Exiting now."];
+    Quit[]
+];
+
+Module[ 
+    {existing, existingLabel, existingVersion,
+     latestLabel, latestVersion,
+     input},
+
+    Print["************************************************************************"];
+    Print["***      N C W e b U p d a t e: a web autoupdater for NCAlgebra      ***"];
+    Print["************************************************************************"];
+    
+    (* Check for existing installations *)
+    existing = FindFile["NC`"];
+    If [ existing =!= $Failed,
+         existing = DirectoryName[existing];
+         {existingLabel, existingVersion} = 
+           Import[FileNameJoin[{existing, 
+                               "NC_VERSION"}]][[1, {1,2}]];
+         existingVersion = StringTrim[existingVersion];
+         
+         Print["\n> Found an installation of"];
+         Print["  ", existingLabel, " ", existingVersion];
+         Print["  already in the directory"];
+         Print["  ", existing];
+         
+        ,
+         Print["\n > Could not find NCAlgebra installation."];
+         Print["  Aborting..."];
+         Return[];
+    ];
+
+    (* Retrieve latest NC_VERSION *)
+    {latestLabel, latestVersion} = 
+       Import["https://raw.githubusercontent.com/NCAlgebra/NC/master/NC_VERSION"][[1, {1,2}]];
+    latestVersion = StringTrim[latestVersion];
+
+    If[ latestVersion != existingVersion
+       ,
+
+         Print["\n> Latest version available is:"];
+         Print["  ", latestLabel, " ", latestVersion];
+               
+       ,
+         Print["\n> You are already running the latest version of NCAlgebra"];
+         Print["  Exiting..."];
+         Return[];
+    ];
+        
+    input = "Z";
+    While[ !(input == "Y" || input == "N")
+          ,
+           input = ToUpperCase[
+             InputString["  Do you want to upgrade? (y/n) "]];
+   ];
+   If[ input == "Y"
+      ,
+       Print["> Upgrading..."];
+       Print[];
+       Import["https://raw.githubusercontent.com/NCAlgebra/NC/master/NCExtras/NCWebInstall.m"];
+      ,
+       Print["  Exiting..."];
+   ];
+       
+];
